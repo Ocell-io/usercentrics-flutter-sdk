a761e8b3e9c17a9e00d2d2ea4af56366a74b03d2
Bogdan Gheorghiu <bogdangheorghiu7@gmail.com>
2025-11-18 10:30:16 +0200

    Update web functionalities

--- lib/src/web/events/usercentrics_web_close_event.dart
+++ lib/src/web/events/usercentrics_web_close_event.dart
@@ -5,15 +5,16 @@ import 'package:web/web.dart' as web;
 import 'dart:js_interop';
 
 class UsercentricsWebCloseEvent {
-  static const String _windowCloseEventName = 'UC_UI_CMP_EVENT';
+  static const String _windowCloseEventName = 'UC_UI_VIEW_CHANGED';
+  static const String _windowInteractionEventName = 'UC_UI_CMP_EVENT';
 
   final Completer<UsercentricsUserInteraction> _completer =
       Completer<UsercentricsUserInteraction>();
 
   late final JSFunction _closeCallback;
+  late final JSFunction _interactionCallback;
 
-  UsercentricsUserInteraction _userInteraction =
-      UsercentricsUserInteraction.noInteraction;
+  UsercentricsUserInteraction? _userInteraction;
 
   Future<UsercentricsUserInteraction> get userInteraction => _completer.future;
 
@@ -21,8 +22,44 @@ class UsercentricsWebCloseEvent {
 
   bool cmpShown = false;
 
+  void disposeView() =>
+      Future.delayed(const Duration(milliseconds: 500), dispose);
+
   UsercentricsWebCloseEvent() {
     _closeCallback = ((web.Event e) {
+      final custom = e as web.CustomEvent;
+      final mapDetails = custom.detail.dartify();
+
+      if (mapDetails is! Map) {
+        return;
+      }
+
+      final view = mapDetails['view'];
+
+      if (view is! String) {
+        return;
+      }
+
+      switch (view) {
+        case 'NONE':
+          if (cmpShown) {
+            disposeView();
+          }
+          break;
+        case 'PRIVACY_BUTTON':
+          if (cmpShown) {
+            disposeView();
+          }
+          break;
+        default:
+          cmpShown = true;
+          break;
+      }
+    }).toJS;
+
+    addWindowEventListener(_windowCloseEventName.toJS, _closeCallback);
+
+    _interactionCallback = ((web.Event e) {
       final custom = e as web.CustomEvent;
       final mapDetails = custom.detail.dartify();
 
@@ -36,37 +73,34 @@ class UsercentricsWebCloseEvent {
         return;
       }
 
-      if (cmpShown) {
-        switch (type) {
-          case 'CMP_ELIGIBLE':
-            Future.delayed(const Duration(milliseconds: 500), dispose);
-            break;
-          case 'ACCEPT_ALL':
-            _userInteraction = UsercentricsUserInteraction.acceptAll;
-            break;
-          case 'DENY_ALL':
-            _userInteraction = UsercentricsUserInteraction.denyAll;
-            break;
-          case 'SAVE':
-            _userInteraction = UsercentricsUserInteraction.granular;
-            break;
-          default:
-            break;
-        }
-      } else if (type == 'CMP_SHOWN') {
-        cmpShown = true;
+      switch (type) {
+        case 'ACCEPT_ALL':
+          _userInteraction = UsercentricsUserInteraction.acceptAll;
+          break;
+        case 'DENY_ALL':
+          _userInteraction = UsercentricsUserInteraction.denyAll;
+          break;
+        case 'SAVE':
+          _userInteraction = UsercentricsUserInteraction.granular;
+          break;
+        default:
+          break;
       }
     }).toJS;
 
-    addWindowEventListener(_windowCloseEventName.toJS, _closeCallback);
+    addWindowEventListener(
+        _windowInteractionEventName.toJS, _interactionCallback);
   }
 
   void dispose() {
     if (!_disposed) {
       _disposed = true;
       removeEventListener(_windowCloseEventName.toJS, _closeCallback);
+      removeEventListener(
+          _windowInteractionEventName.toJS, _interactionCallback);
       if (!_completer.isCompleted) {
-        _completer.complete(_userInteraction);
+        _completer.complete(
+            _userInteraction ?? UsercentricsUserInteraction.noInteraction);
       }
     }
   }
